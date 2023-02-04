0cf3fa8c82eebf21577df4d37f54960fcfbe1e69
Hmmlov <miguelcondegarcia26@gmail.com>
2023-02-04 12:44:34 -0500

    ThunksAxiosPokemonSlice

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,11 +1,21 @@
+import { pokemonApi } from "../../../api/PokemonApi";
 import { setPokemons, startLoadingPokemons } from "./pokemonSlice"
 
 
 export  const getPokemons = ( page = 0 ) => {
     return async(dispatch, getState) => {
         dispatch(startLoadingPokemons() );
+        //Todo: realizar petición http
 
+        /* Forma con Axios */
+        const {data} = await pokemonApi.get(`pokemon?limit=10&offset=${page*10}`)
+
+        /* Otra forma con fecth */
+       /* const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${page*10}`);
+       const data = await resp.json();
+       console.log(data); */
         //data-pokemons 
-        /* dispatch(setPokemons() ); */
+
+        dispatch(setPokemons({pokemons: data.results, page: page + 1 }) );
     } /* https://pokeapi.co/api/v2/pokemon?limit=10&offset=0 */
 }
