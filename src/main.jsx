af6317d9f139b084fef892806d80339ed84e15bf
Hmmlov <miguelcondegarcia26@gmail.com>
2023-02-04 22:47:20 -0500

    TodosApiStoreTodoApp

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,14 @@ import './index.css';
 import { store } from './store/index';
 import { Provider } from 'react-redux';
 import { PokemonApp } from './PokemonApp';
+import { TodoApp } from './TodoApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
      {/*  <App /> */}
-      <PokemonApp />
+      {/* <PokemonApp /> */}
+      <TodoApp /> 
     </Provider>
   </React.StrictMode>
 )
